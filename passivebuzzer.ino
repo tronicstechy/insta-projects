4c23a3d5fb49880dc5c7751d2abc8a493298d514
tronicstechy <40654042+tronicstechy@users.noreply.github.com>
2018-06-28 12:16:12 +0530

    Add files via upload

---
+++ passivebuzzer.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(12, OUTPUT);  //I've used pin 12. The pin is your choice.
+}
+
+void loop() {
+  tone(12, 100);
+  delay(1000);
+
+  tone(12, 200); 
+  delay(1000);
+
+  tone(12, 300); 
+  delay(1000);
+
+  tone(12, 350); 
+  delay(1000);
+
+  tone(12, 360); 
+  delay(1000);
+}
+
